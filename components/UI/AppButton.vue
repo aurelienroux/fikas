e50f68e112fd86815b36d2951e7c582122dc89c6
Aurélien Roux <aureroux13@gmail.com>
2020-05-17 12:35:17 -0400

    button ui element

---
+++ components/UI/AppButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <button class="button" :class="btnStyle">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    btnStyle: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  border-radius: 3rem;
+  border: solid 0.2rem $perrywinkle;
+  color: $perrywinkle;
+  cursor: pointer;
+  font-family: $font-secondary;
+  font-size: 1.4rem;
+  font-weight: 700;
+  margin: 1rem;
+  padding: 2rem 3rem;
+  transition: 0.3s;
+
+  &:hover {
+    background-color: $perrywinkle;
+    color: $white;
+  }
+
+  &--full {
+    background-color: $perrywinkle;
+    color: $white;
+
+    &:hover {
+      background-color: $white;
+      color: $perrywinkle;
+    }
+  }
+}
+</style>
---
+++ plugins/global.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import AppButton from '~/components/UI/AppButton.vue'
+
+Vue.component('AppButton', AppButton)
