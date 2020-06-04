ef552fbe4d60ee0fbd30f55e51e4e9b5cf498d6a
Aurélien Roux <aureroux13@gmail.com>
2020-06-03 21:48:01 -0400

    Functional footer  (#17)
    * F/the footer(#6) (#16)
    
    * mobile footer
    
    * mobile footer
    
    Co-authored-by: Aurélien Roux <aurelien.roux@aerial.ai>

--- pages/index.vue
+++ pages/index.vue
@@ -2,27 +2,282 @@
   <div class="container">
     <div>
       <AppBtn>test btn</AppBtn>
-      <AppBtn btn-style="button--full">S'inscrire a l infolettre</AppBtn>
-      <AppBtnYoutube />
-      <AppBtnYoutube btn-style="link--footer" />
-      <AppBtnFacebook />
-      <AppBtnFacebook btn-style="link--footer" />
-      <AppBtnInsta />
-      <AppBtnInsta btn-style="link--footer" />
-
-      <div style="background: red; padding: 2rem">
-        <AppIconMore />
-        <AppIconPrev />
-        <AppIconNext />
-        <AppIconMenuDrop />
-        <AppIconVideoScroll />
-        <AppIconClose />
-        <AppIconClose icon-style="icon--menu" />
-        <AppLogoFooter />
-        <AppLogo />
-        <div class="bg"></div>
-        <div class="bg-two"></div>
-      </div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
+        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
+        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
+        cumque dolorum tempora!
+      </p>
     </div>
   </div>
 </template>
@@ -30,24 +285,3 @@
 <script>
 export default {}
 </script>
-
-<style>
-.bg {
-  background-image: url('~assets/images/bg1.svg');
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  border: 1px solid black;
-  height: 20rem;
-  width: 20rem;
-}
-.bg-two {
-  background-image: url('~assets/images/bg2.svg');
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  border: 1px solid black;
-  height: 43rem;
-  width: 192rem;
-}
-</style>
