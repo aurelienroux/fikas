0e828283c8647d9c5051899a1774e33642bae51f
Aurélien Roux <aureroux13@gmail.com>
2020-06-29 17:22:10 -0400

    h/fix lang button style

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -174,15 +174,15 @@ export default Vue.extend({
   }
 
   & .btn-lang {
+    align-items: center;
     background: transparent;
-    border: none;
     color: $white;
-    cursor: pointer;
-    font-weight: bold;
-    height: 100%;
+    display: flex;
     padding: 0 2rem;
     position: relative;
     right: -2rem;
+    text-decoration: none;
+    text-transform: uppercase;
   }
 
   .menu-sublink.nuxt-link-exact-active {
