d93023cf491f1a3728a4078834da6a265f9562e5
Aurélien Roux <aureroux13@gmail.com>
2020-06-30 16:24:21 -0400

    page and hero SB component (#28)

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,32 @@
 <template>
-  <div class="container">
-    <div>
-      <AppBtn>test btn</AppBtn>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
-        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
-        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
-        cumque dolorum tempora!
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ex
-        porro natus voluptates quae aliquid eaque incidunt neque cupiditate.
-        Illo repudiandae reprehenderit consequuntur in, ipsam dolores iure
-        cumque dolorum tempora!
-      </p>
-      <p>{{ story.content.title }}</p>
-    </div>
-  </div>
+  <main class="container">
+    <component
+      :is="story.content.component"
+      v-if="story.content.component"
+      :key="story.content._uid"
+      :blok="story.content"
+    ></component>
+  </main>
 </template>
 
 <script>
 export default {
   name: 'Homepage',
   asyncData(context) {
-    // Load the JSON from the API
     return context.app.$storyapi
       .get(`cdn/stories/${context.app.i18n.locale}/home`, {
         version: 'draft'
       })
       .then((res) => {
-        // console.log(context.app.i18n.locale)
         return res.data
       })
       .catch((res) => {
         if (!res.response) {
-          // console.error(res)
           context.error({
             statusCode: 404,
             message: 'Failed to receive content form api'
           })
         } else {
-          // console.error(res.response.data)
           context.error({
             statusCode: res.response.status,
             message: res.response.data
@@ -54,14 +40,12 @@ export default {
     }
   },
   mounted() {
-    // use the bridge to listen to events
     this.$storybridge.on(['input', 'published', 'change'], (event) => {
       if (event.action === 'input') {
         if (event.story.id === this.story.id) {
           this.story.content = event.story.content
         }
       } else {
-        // window.location.reload()
         this.$nuxt.$router.go({
           path: this.$nuxt.$router.currentRoute,
           force: true
