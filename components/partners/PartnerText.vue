7884df1806b23354d31f4fcab82f3853beaa49f6
Aurélien Roux <aurelien.roux@aerial.ai>
2020-07-30 19:43:51 -0400

    partners blocks

---
+++ components/partners/PartnerText.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="partner-text" v-html="richtext"></div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    blok: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  computed: {
+    richtext() {
+      return this.blok.text
+        ? this.$storyapi.richTextResolver.render(this.blok.text)
+        : ''
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.partner-text {
+  flex-basis: calc(50% - 2rem);
+  font-family: $font-primary;
+  font-size: 2rem;
+  margin: 1rem;
+  min-width: 300px;
+}
+</style>
+
+<style lang="scss">
+.partner-text {
+  a {
+    text-decoration: none;
+  }
+
+  .link-styling {
+    color: $perrywinkle;
+    text-decoration: none;
+  }
+}
+</style>
---
+++ components/partners/Partners.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="partners">
+    <h2 class="partners__title">
+      {{ blok.title }}
+      <div class="layout-bar"></div>
+    </h2>
+    <div class="partners__container">
+      {{ blok.partners }}
+      <component
+        :is="blok.component"
+        v-for="blok in blok.components"
+        :key="blok._uid"
+        :blok="blok"
+      ></component>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    blok: {
+      type: Object,
+      default: () => {}
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.partners {
+  max-width: 120rem;
+  padding: 7rem 2rem;
+  position: relative;
+  width: 100%;
+
+  &__title {
+    color: $aqua-blue;
+    display: inline-block;
+    font-family: $font-secondary;
+    font-size: 4rem;
+    font-weight: 600;
+    margin-bottom: 4rem;
+    position: relative;
+    text-transform: uppercase;
+
+    .layout-bar {
+      background-color: $light-teal;
+      display: none;
+      height: 0.2rem;
+      position: absolute;
+      right: -9rem;
+      top: 3.5rem;
+      width: 8rem;
+
+      @include for-tablet-landscape-up {
+        display: initial;
+      }
+    }
+  }
+
+  &__container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+}
+</style>
