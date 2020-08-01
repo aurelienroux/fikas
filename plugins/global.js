a64550ed7158a0124a503b95a36ee9ce2b1cc40a
Aurélien Roux <aureroux13@gmail.com>
2020-08-01 17:55:49 -0400

    festival video player and history components (#50)
    * F/festival page (#49)
    
    * History blocks
    * video player component
    * add button to video player
    * correct naming component
    
    close #49 
    close #48

--- plugins/global.js
+++ plugins/global.js
@@ -30,6 +30,10 @@ import Pinkblock from '~/components/Pinkblock.vue'
 import Partners from '~/components/partners/Partners.vue'
 import PartnerImage from '~/components/partners/PartnerImage.vue'
 import PartnerText from '~/components/partners/PartnerText.vue'
+import History from '~/components/history/History.vue'
+import HistoryLeft from '~/components/history/HistoryLeft.vue'
+import HistoryRight from '~/components/history/HistoryRight.vue'
+import VideoPlayback from '~/components/VideoPlayback.vue'
 
 Vue.component('Btn', Btn)
 Vue.component('BtnFacebook', BtnFacebook)
@@ -62,3 +66,7 @@ Vue.component('Pinkblock', Pinkblock)
 Vue.component('Partners', Partners)
 Vue.component('PartnerImage', PartnerImage)
 Vue.component('PartnerText', PartnerText)
+Vue.component('History', History)
+Vue.component('HistoryLeft', HistoryLeft)
+Vue.component('HistoryRight', HistoryRight)
+Vue.component('VideoPlayback', VideoPlayback)
