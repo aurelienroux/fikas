37358395b5bb4121e6f5f1754c4d34eefde1f66c
Aurélien Roux <aureroux13@gmail.com>
2020-08-18 20:42:27 -0400

    contact form with i18n strings

--- components/contact/ContactForm.vue
+++ components/contact/ContactForm.vue
@@ -8,13 +8,24 @@
     >
       <input type="hidden" name="form-name" value="contact-form" />
       <input type="hidden" name="bot-field" />
-      <input type="text" name="FirstName:" placeholder="first" required />
-      <input type="text" name="LastName:" placeholder="last" required />
-      <input type="email" name="Email:" placeholder="email" required />
+      <input type="text" name="FirstName:" :placeholder="$t('contact.first')" />
+      <input
+        type="text"
+        name="LastName:"
+        :placeholder="$t('contact.last')"
+        required
+      />
+      <input
+        type="email"
+        name="Email:"
+        :placeholder="$t('contact.email')"
+        required
+      />
       <select name="Subject:">
-        <option selected>Question generales</option>
-        <option>benevol</option>
-        <option>Partneriat</option>
+        <option disabled selected>{{ $t('contact.selectSubject') }}</option>
+        <option>{{ $t('contact.general') }}</option>
+        <option>{{ $t('contact.volunteer') }}</option>
+        <option>{{ $t('contact.partner') }}</option>
       </select>
       <textarea name="Message:" rows="4" cols="50" />
 
@@ -33,5 +44,59 @@ export default Vue.extend({})
 .contact-form {
   border: 1px solid;
   position: relative;
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  input[type='text'],
+  input[type='email'],
+  select,
+  textarea {
+    background-color: transparent;
+    border-radius: 1rem;
+    border: solid 0.2rem $perrywinkle;
+    color: $charcoal-grey;
+    font-family: $font-primary;
+    font-size: 2rem;
+    margin-bottom: 2rem;
+    padding-left: 2rem;
+
+    &::placeholder {
+      color: rgba(48, 52, 66, 0.7);
+    }
+  }
+
+  input[type='text'],
+  input[type='email'],
+  select {
+    height: 6rem;
+  }
+
+  textarea {
+    padding: 2rem;
+  }
+
+  input[type='submit'] {
+    align-self: flex-end;
+    background-color: $perrywinkle;
+    border-radius: 3rem;
+    border: solid 0.2rem $perrywinkle;
+    color: $white;
+    cursor: pointer;
+    display: inline-block;
+    font-family: $font-secondary;
+    font-size: 1.4rem;
+    font-weight: 700;
+    margin: 1rem 0 1rem 1rem;
+    padding: 2rem 3rem;
+    transition: 0.3s;
+
+    &:hover {
+      background: transparent;
+      color: $perrywinkle;
+    }
+  }
 }
 </style>
