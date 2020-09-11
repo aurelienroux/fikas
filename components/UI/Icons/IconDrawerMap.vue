c8413c40f720daca99ff42e10ca636cb0e2a676a
Aurélien Roux <aureroux13@gmail.com>
2020-09-11 16:38:28 -0400

    F/country map (#84)
    * countries map mobile
    * countries container max width and title
    * functional desktop country map
    * countries map as components
    * correct countries desc positioning
    * refacto code
    * css order

---
+++ components/UI/Icons/IconDrawerMap.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="icon" :class="{ open: iconStyle }" v-on="$listeners">
+    <div class="bar bar--one"></div>
+    <div class="bar bar--two"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    iconStyle: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  height: 4rem;
+  position: relative;
+  width: 4rem;
+
+  & .bar {
+    background-color: $white;
+    border-radius: 0.1rem;
+    position: absolute;
+    transition: 0.3s;
+  }
+
+  .bar--one {
+    height: 0.8rem;
+    top: calc(50% - 0.4rem);
+    width: 4rem;
+  }
+
+  .bar--two {
+    height: 4rem;
+    left: calc(50% - 0.4rem);
+    width: 0.8rem;
+  }
+
+  &.open .bar--two {
+    display: none;
+  }
+}
+</style>
---
+++ components/countries/Countries.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="countries">
+    <h2 class="countries__title">
+      {{ blok.title }}
+      <div class="layout-bar"></div>
+    </h2>
+    <div class="countries__mobile">
+      <img
+        class="countries__image"
+        src="~/assets/images/map-pays.png"
+        alt="countries map"
+      />
+      <Drawer
+        v-for="country in blok.country"
+        :key="country.title"
+        :title="country.title"
+        :color="country.background === true ? 'drawer--purple' : ''"
+        :content="country.content"
+      />
+    </div>
+    <div class="countries__desktop">
+      <Map @select-country="selectCountry" />
+      <Description
+        v-for="country in blok.country"
+        :id="country.id"
+        :key="country.title"
+        :display-country="displayCountry"
+        :content="country.content"
+        @hide-country="hideCountry"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Drawer from '@/components/countries/DrawerMap'
+import Map from '@/components/countries/Map'
+import Description from '@/components/countries/Description'
+
+export default Vue.extend({
+  components: {
+    Drawer,
+    Map,
+    Description
+  },
+  props: {
+    blok: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      displayCountry: null
+    }
+  },
+  methods: {
+    selectCountry(country) {
+      this.displayCountry = country
+    },
+    hideCountry() {
+      this.displayCountry = null
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.countries {
+  margin: 8rem 0;
+  max-width: 120rem;
+  position: relative;
+
+  &__title {
+    color: $aqua-blue;
+    font-family: $font-secondary;
+    font-size: 4rem;
+    font-weight: 600;
+    margin: 0 2rem 4rem;
+    text-transform: uppercase;
+
+    @include for-big-desktop-up {
+      left: -20rem;
+      position: absolute;
+      top: 25rem;
+      transform: rotate(-90deg);
+    }
+
+    .layout-bar {
+      display: none;
+
+      @include for-big-desktop-up {
+        background-color: $light-teal;
+        display: initial;
+        height: 0.2rem;
+        position: absolute;
+        right: -9rem;
+        top: 3.5rem;
+        width: 8rem;
+      }
+    }
+  }
+
+  &__image {
+    margin-bottom: 4rem;
+  }
+
+  &__mobile {
+    display: initial;
+
+    @include for-desktop-up {
+      display: none;
+    }
+  }
+
+  &__desktop {
+    display: none;
+
+    @include for-desktop-up {
+      display: initial;
+    }
+  }
+}
+</style>
