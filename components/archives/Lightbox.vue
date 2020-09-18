d529d40ebd26e6042865130ca24da0e1e4a319bb
Aurélien Roux <aureroux13@gmail.com>
2020-09-18 16:34:33 -0400

    lightbox

---
+++ components/archives/Lightbox.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="lightbox">
+    <button @click="$emit('closeLightbox')"><IconClose /></button>
+    <div class="container">
+      <img class="lightbox__image" :src="image.image.filename" :alt="image" />
+      <p class="lightbox__credits">{{ image.credits }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    image: {
+      type: Object,
+      default: () => null
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.lightbox {
+  align-items: center;
+  background: rgba($white, 0.9);
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: 1000;
+
+  .container {
+    margin: 2rem;
+    max-width: 90rem;
+  }
+
+  button {
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    padding: 4rem;
+    position: absolute;
+    right: 2rem;
+    top: 2rem;
+
+    svg {
+      fill: $perrywinkle;
+    }
+  }
+
+  &__credits {
+    color: $charcoal-grey;
+    font-family: $font-primary;
+    font-size: 2.4rem;
+    line-height: 1.33;
+    margin-top: 3rem;
+  }
+}
+</style>
