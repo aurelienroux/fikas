4989c52e582291f271ffb064be3f4176486cc3ff
Aurélien Roux <aureroux13@gmail.com>
2020-09-22 14:13:05 -0400

    change global components file name

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [{ src: '~/plugins/global.js' }],
+  plugins: [{ src: '~/plugins/globalComponents.js' }],
   /*
    ** Nuxt.js dev-modules
    */
