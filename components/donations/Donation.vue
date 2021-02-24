8763b10744e0dd5eb89a915ccc9783206d90fc06
Aurélien Roux <aureroux13@gmail.com>
2021-02-24 11:59:48 -0500

    donations page (#103)
    * donations page

---
+++ components/donations/Donation.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="donation">
+    <h2 v-if="blok.title" class="donation__title">{{ blok.title }}</h2>
+    <p v-if="blok.content" class="donation__content">
+      {{ blok.content }}
+    </p>
+    <img
+      v-if="blok.image.filename"
+      class="donation__img"
+      :src="blok.image.filename"
+      :alt="blok.image.filename"
+    />
+    <div>
+      <a
+        class="donation__btn"
+        target="_blank"
+        rel="noopener noreferrer"
+        :href="blok.link.cached_url"
+      >
+        <Btn>{{ blok.button_text }}</Btn>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.donation {
+  display: flex;
+  flex-direction: column;
+  font-family: $font-primary;
+  font-size: 2rem;
+  text-align: center;
+
+  &__title {
+    color: $aqua-blue;
+    font-family: $font-secondary;
+    font-size: 2.4rem;
+    margin-bottom: 1rem;
+    text-transform: uppercase;
+  }
+
+  &__content {
+    flex: 1;
+    line-height: 1.4;
+    margin-bottom: 2rem;
+    margin: 0 auto 1rem;
+    max-width: 60rem;
+    text-align: left;
+  }
+
+  &__img {
+    height: 25rem;
+    margin: 0 auto 1.5rem;
+    max-width: 60rem;
+    object-fit: cover;
+    object-position: center;
+    width: 100%;
+  }
+}
+</style>
